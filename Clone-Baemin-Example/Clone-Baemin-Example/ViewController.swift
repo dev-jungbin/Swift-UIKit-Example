e7c398e1313c77641ea6128f55d9bdc9e145d1d3
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-15 21:09:34 +0900

    [CHORE] Create Baemin Project

---
+++ Clone-Baemin-Example/Clone-Baemin-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone-Baemin-Example
+//
+//  Created by 1v1 on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FCM-example/Pods/Target Support Files/Pods-FCM-example/Pods-FCM-example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FCM_example : NSObject
+@end
+@implementation PodsDummy_Pods_FCM_example
+@end
