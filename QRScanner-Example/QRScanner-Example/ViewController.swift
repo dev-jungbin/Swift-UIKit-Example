be0c3c9d0eca5fc27276c3b0e291c67623b7d377
Jungbin Jung <43312096+dev-madilyn@users.noreply.github.com>
2021-10-25 19:43:12 +0900

    ### 제목 - 최대 50 글자 [CHORE] Practice QRScanner
    ### 본문 - 한 줄에 최대 72 글자
    
    ### 푸터 - etc. Closed #Issue_Number
    
    # --- COMMIT END ---
    # [CHORE]: 코드 수정, 내부 파일 수정
    # [FEAT]: 새로운 기능 구현
    # [ADD]: Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성, 에셋 추가 등
    # [HOTFIX]: issue나, QA에서 급한 버그 수정에 사용
    # [FIX]: 버그, 오류 해결
    # [DEL]: 쓸모없는 코드 삭제
    # [DOCS]: README나 WIKI 등의 문서 개정
    # [MOD]: storyboard 파일, UI 수정한 경우
    # [CORRECT]: 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다.
    # [MOVE]: 프로젝트 내 파일이나 코드의 이동
    # [RENAME]: 파일 이름 변경이 있을 때 사용합니다.
    # [IMPROVE]: 향상이 있을 때 사용합니다.
    # [REFACTOR]: 전면 수정이 있을 때 사용합니다.
    # [MERGE]: 다른 브랜치를 merge 할 때 사용합니다.
    # --- vim 단축키 ---
    # 선택: v+커서 (비주얼 모드)
    # 복사: y
    # 붙여넣기: p
    # 편집(insert): i
    # 저장 후 종료: esc -> :wq
    
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

---
+++ QRScanner-Example/QRScanner-Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScanner-Example
+//
+//  Created by 1v1 on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
