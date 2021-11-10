887111d1dddd9d9c8ce19287470e54b8af4ae940
Jungbin Jung <lollin0@naver.com>
2021-11-10 14:50:19 +0900

    Practice EasyTipView

--- EasyTipView-Example/EasyTipView-Example/ViewController.swift
+++ EasyTipView-Example/EasyTipView-Example/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import EasyTipView
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var showBtn: UIButton!
+    @IBOutlet weak var bellBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapShowBtn(_ sender: Any) {
+        var preferences = EasyTipView.globalPreferences
+        preferences.drawing.foregroundColor = UIColor.white
+        preferences.drawing.backgroundColor = UIColor.blue
+//        preferences.drawing.font = UIFont(name: "HelveticaNeue-Light", size: 14)!
+        preferences.drawing.textAlignment = NSTextAlignment.justified
+        
+        preferences.animating.dismissTransform = CGAffineTransform(translationX: 0, y: -15)
+        preferences.animating.showInitialTransform = CGAffineTransform(translationX: 0, y: 15)
+        preferences.animating.showInitialAlpha = 0
+        preferences.animating.showDuration = 1
+        preferences.animating.dismissDuration = 1
+        preferences.drawing.arrowPosition = .top
+        
+        let text = "알림 도착"
+        EasyTipView.show(forView: self.bellBtn,
+            withinSuperview: self.navigationController?.view,
+            text: text,
+            preferences: preferences)
 
+    }
+    
 }
 
