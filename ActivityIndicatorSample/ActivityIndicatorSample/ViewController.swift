0a8701240309f1c1e1d834a8e086490d8551b7d8
Jungbin Jung <lollin0@naver.com>
2021-12-31 16:09:02 +0900

    Chore: ActivityIndicator 실습 with BaseVC

--- ActivityIndicatorSample/ActivityIndicatorSample/ViewController.swift
+++ ActivityIndicatorSample/ActivityIndicatorSample/ViewController.swift
@@ -7,31 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseVC {
     @IBOutlet weak var completeView: UIView!
-    lazy var activityIndicator: UIActivityIndicatorView = {
-        // 해당 클로저에서 나중에 indicator 를 반환해주기 위해 상수형태로 선언
-        let activityIndicator = UIActivityIndicatorView()
-        
-        activityIndicator.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        
-        activityIndicator.center = self.view.center
-        
-        // 기타 옵션
-        activityIndicator.color = .purple
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.style = .medium
-        
-        // stopAnimating을 걸어주는 이유는, 최초에 해당 indicator가 선언되었을 때, 멈춘 상태로 있기 위해서
-        activityIndicator.stopAnimating()
-        
-        return activityIndicator
-        
-    }()
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(activityIndicator)
         
         completeView.isHidden = true
         DispatchQueue.main.async {
@@ -45,3 +27,18 @@ class ViewController: UIViewController {
     }
 }
 
+func setActivityIndicator(view: UIView) -> UIActivityIndicatorView {
+    let activityIndicator = UIActivityIndicatorView()
+    
+    activityIndicator.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    
+    activityIndicator.center = view.center
+    
+    // 기타 옵션
+    activityIndicator.color = .purple
+    activityIndicator.hidesWhenStopped = true
+    activityIndicator.style = .medium
+    
+    activityIndicator.stopAnimating()
+    return activityIndicator
+}
