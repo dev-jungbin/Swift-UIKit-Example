f91e0382c474d321d041b8bc58a3e071e0158a45
Jungbin Jung <lollin0@naver.com>
2022-01-06 00:37:13 +0900

    Chore: TextField, TextView 디자인 적용

--- WriteView-Example/WriteView-Example/ViewController.swift
+++ WriteView-Example/WriteView-Example/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var changeMajorBtn: UIButton!
+    @IBOutlet weak var oneLineTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    private func configureUI() {
+        oneLineTextField.addLeftPadding(16)
+        oneLineTextField.addRightPadding(16)
+        textView.textContainerInset = UIEdgeInsets(top: 16, left: 12, bottom: 16, right: 12)
+        [oneLineTextField, textView].forEach { view in
+            view?.layer.borderWidth = 1
+            view?.layer.addRadius(radius: 8)
+            view?.layer.borderColor = UIColor.lightGray.cgColor
+        }
+
     }
     
     @IBAction func tapChangeMajorBtn(_ sender: Any) {
@@ -40,3 +54,33 @@ class ViewController: UIViewController {
     
 }
 
+extension UITextField {
+    ///왼쪽에 여백 주기
+    func addLeftPadding(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    ///오른쪽에 여백 주기
+    func addRightPadding(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
+
+extension CALayer {
+
+    func addRadius(_ corners: UIRectCorner, radius: CGFloat, view: UIView) {
+        let mask = CAShapeLayer()
+        mask.bounds = view.frame
+        mask.position = view.center
+        mask.path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+        view.layer.mask = mask
+    }
+
+    func addRadius(radius: CGFloat) {
+        self.cornerRadius = radius
+    }
+}
